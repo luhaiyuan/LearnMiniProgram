58af78365078d35f6eedc2d8ec4b07cac44f867e
luhaiyuan <1009369648@qq.com>
2020-03-27 16:40:34 +0800

    知识点3

--- pages/home/home.js
+++ pages/home/home.js
@@ -1,66 +1,29 @@
 // pages/home/home.js
 Page({
-
-  /**
-   * 页面的初始数据
-   */
   data: {
-
+    name: 'Coderwhy',
+    age: 19,
+    students: [
+      { id: 110, name: 'kobe', age: 30 },
+      { id: 111, name: 'james', age: 29 },
+      { id: 112, name: 'curry', age: 36 },
+      { id: 113, name: 'coderwhy', age: 18 }
+    ],
+    counter: 0
   },
+  handleBtnClick() {
+    // 1.错误的做法，界面是不会刷新的
+    // this.data.counter += 1;
+    // console.log(this.data.counter);
 
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-
+    // 2.this.setData()
+    this.setData({
+      counter: this.data.counter + 1
+    })
   },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
+  handleSubtraction() {
+    this.setData({
+      counter: this.data.counter - 1
+    })
   }
 })
