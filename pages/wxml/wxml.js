b914eaa780b2cede838c21a013118fc61afbb065
luhaiyuan <1009369648@qq.com>
2020-04-04 10:20:14 +0800

    11wxs语法

---
+++ pages/wxml/wxml.js
@@ -0,0 +1,9 @@
+// pages/wxml/wxml.js
+Page({
+  data: {
+
+  },
+  numberTofixed(value) {
+    return value.toFixed(2);
+  }
+})
---
+++ pages/wxs/wxs.js
@@ -0,0 +1,7 @@
+// pages/wxs/wxs.js
+Page({
+  data: {
+    price: '25.66666',
+    time: 1585965157940
+  }
+})
