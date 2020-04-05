997489a21bde980fb4a94666fae73b6799e8f2d7
luhaiyuan <1009369648@qq.com>
2020-04-05 21:07:45 +0800

    14网络请求

--- pages/home/home.js
+++ pages/home/home.js
@@ -1,66 +1,79 @@
 // pages/home/home.js
-Page({
+// Page({
 
-  /**
-   * 页面的初始数据
-   */
+//   /**
+//    * 页面的初始数据
+//    */
+//   data: {
+
+//   },
+
+//   /**
+//    * 生命周期函数--监听页面加载
+//    */
+//   onLoad: function (options) {
+//     // 发送网络请求
+//     // 1.发送最简单的GET请求
+//     // wx.request({
+//     //   url: 'http://123.207.32.32:8000/recommend',
+//     //   success: function (res) {
+//     //     console.log(res);
+//     //   }
+//     // })
+
+//     // 2.GET请求携带参数
+//     // wx.request({
+//     //   url: 'http://123.207.32.32:8000/home/data',
+//     //   data: {
+//     //     type: 'sell',
+//     //     page: 1
+//     //   },
+//     //   success: function (res) {
+//     //     console.log(res);
+//     //   }
+//     // })
+
+//     // 3.POST请求并且携带参数
+//     wx.request({
+//       url: 'http://httpbin.org/post',
+//       method: 'POST',
+//       data: {
+//         name: 'coderwhy',
+//         age: 18
+//       },
+//       success: function (res) {
+//         console.log(res);
+//       },
+//       fail: function (err) {
+//         console.log(err);
+//       },
+//       complete() {
+//         // 接口调用完成，触发这个函数，不管成功还是失败
+//         console.log('接口调用完成');
+//       }
+//     })
+//   }
+// })
+
+
+
+// ----------使用封装的request发送网络请求------------
+// Promise最大的好处就是防止出现回调地狱
+import request from '../../service/network.js';
+Page({
   data: {
 
   },
-
-  /**
-   * 生命周期函数--监听页面加载
-   */
   onLoad: function (options) {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
+    // 发送网络请求
+    request({
+      url: 'http://123.207.32.32:8000/recommend'
+    })
+    .then(res => {
+      console.log(res);
+    })
+    .catch(err => {
+      console.log(err);
+    })
   }
 })
---
+++ service/network.js
@@ -0,0 +1,30 @@
+// 简洁的写法
+export default function request(options) {
+  return new Promise((resolve, reject) => {
+    wx.request({
+      url: options.url,
+      method: options.method || 'GET',
+      data: options.data || {},
+      success: resolve,
+      fail: reject
+    })
+  })
+}
+
+
+
+// export default function request(options) {
+//   return new Promise((resolve, reject) => {
+//     wx.request({
+//       url: options.url,
+//       method: options.method || 'GET',
+//       data: options.data || {},
+//       success: function (res) {
+//         resolve(res);
+//       },
+//       fail: function (err) {
+//         reject(err);
+//       }
+//     })
+//   })
+// }
